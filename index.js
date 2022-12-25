df98602f6122c750fd3d60ef19d80d9ffa362389
Andrea Bellver <bellverrha@gmail.com>
2022-12-25 08:26:43 +0100

    Ejercicio hecho

---
+++ index.js
@@ -0,0 +1,13 @@
+const nombre = 'Andrea';
+const apellido = 'Bellver';
+
+const miObj = {
+    nombre: nombre,
+    apellido: apellido,
+}
+
+// sessionStorage.setItem("persona", JSON.stringify(miObj));
+
+// localStorage.setItem("persona", JSON.stringify(miObj));
+
+// document.cookie = "persona=" + JSON.stringify(miObj) + ";max-age=120";
